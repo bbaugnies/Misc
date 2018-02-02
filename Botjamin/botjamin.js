f342758f3a75b2fac946fd6f406e008c51dc82e2
Benjamin Baugnies <benjamin.baugnies@cert.be>
2018-02-02 14:20:23 +0100

    update botjamin

--- Botjamin/botjamin.js
+++ Botjamin/botjamin.js
@@ -1,11 +1,22 @@
 const login = require("facebook-chat-api");
 var fs = require("fs");
-var regex = /^\/[rR](oll)?(\d+)d(\d+)( ?[+-]\d+)?(.*)/
+//var regex = /^\/[rR](oll)?(\d+)d(\d+)( ?[+-]\d+)?(.*)/
+var regex = /^\/[rR](oll)? ?(\d+)d(\d+)/
 
 var debug = false
+var override = false
 
 var pwd = fs.readFileSync("./pwd.txt", "utf8")
 pwd = pwd.substring(0, pwd.length - 1)
+
+
+ids = {
+    '740895416': "ben",
+    '1440180349': "coco",
+    '739582489': "finf",
+    '1094374602': "gog"
+}
+
 
 var ben = '740895416'
 var corentin = '1440180349'
@@ -15,59 +26,20 @@ var gauthier = '1094374602'
 var warhammer = '1600281336648636'
 var sw = '456732981199984'
 
-function processRes(res, g_mod, opt) {
-    values = "[" + res.toString().replace(/,/g, ", ")+"]"
-    options = opt.match(/-([a-zA-Z]*) ?([^ -]*)?/g)
-    post = "\n\n"
-    if (debug) console.log(g_mod);
-    if (debug) console.log(options);
-    for (i = 0; i<options.length; i++) {
-        m = options[i].match(/-([a-zA-Z]*) ?([^ -]*)?/)
-        if (debug) console.log(m);
-        switch(m[1]) {
-            case "sum":
-                sum = res.reduce((t, n) => {return t+n})
-                if (g_mod) {
-                    if (g_mod[g_mod.length-2] == '+') sum += parseInt(g_mod[g_mod.length-1]);
-                    else if (g_mod[g_mod.length-2] == '-') sum -= parseInt(g_mod[g_mod.length-1]);
-                }
-                post += "Sum: " + sum + "\n"
-                break;
-            case "avg":
-                avg = res.reduce((t, n) => {return t+n})/res.length
-                post += "Average: " + avg + "\n"
-                break;
-            case "over":
-                if (m[2].match(/^\d+$/)) {
-                    count_over = 0
-                    for (i = 0; i < res.length; i++) {
-                        if (res[i] > parseInt(m[2])) count_over ++;
-                    }
-                    post += "Rolls over " + m[2] +": " + count_over + "\n";
-                }
-                else { post += "You messed up, human\n"; }
-                break;
-            case "under":
-                if (m[2].match(/^\d+$/)) {
-                    count_over = 0
-                    for (i = 0; i < res.length; i++) {
-                        if (res[i] < parseInt(m[2])) count_over ++;
-                    }
-                    post += "Rolls over " + m[2] +": " + count_over + "\n";
-                }
-                else  { post += "You messed up, human\n"; }
-                break;
-            default:
-                post += "You messed up, human\n";
-        }
-    }
-    if (debug) console.log(values + post);
-    return (values + post)
+top = {
+    coco: null,
+    finf: null,
+    ben: null,
+    gog: null
 }
+
 
 function runBot(msg) {
     login({email: "botgnies@gmail.com", password: pwd}, (err, api) => {
-        if(err) { console.log(err); }
+        if(err) {
+            api.sendMessage("So... Cold...", ben);
+            console.log(err);            
+        }
         api.sendMessage(msg, ben);
         if (!debug){
             api.sendMessage(msg, warhammer);
@@ -75,7 +47,6 @@ function runBot(msg) {
         }
         
         api.listen((err, message) => {
-            console.log(message.senderID)
             if (err) { console.log(err); }
             else {
 	            m = message.body.match(regex)
@@ -87,13 +58,18 @@ function runBot(msg) {
                         api.sendMessage("Bad Human!", message.threadID);
                     }
                     else {
-                        res = []
-		                for (i = 0; i < n_dice; i++) {
-			                res.push(Math.floor((Math.random() * s_dice) + 1))
-		                }
-		                r = processRes(res,m[4], m[5]);
-		                api.sendMessage(r, message.threadID);
-	                }
+                        if (top[ids[message.senderID]] != null && override && m[2] == top[ids[message.senderID]][1] && m[3] == top[ids[message.senderID]][2]) {
+                            api.sendMessage(top[ids[message.senderID]][0], message.threadID);
+                            top[ids[message.senderID]] = top[ids[message.senderID]][3]
+                        }
+                        else {
+                            for (i = 0; i < n_dice; i++) {
+		                    r += Math.floor((Math.random() * s_dice) + 1)
+		                    r += ", "
+     	                    }
+	                        api.sendMessage(r.substring(0, r.length-2), message.threadID);
+                        }
+                    }
 	            }
 	            else if (message.body.match(/^[gG]ood bot/)) {
 		            api.sendMessage("Thank you, human "+ message.senderID, message.threadID);
@@ -116,6 +92,44 @@ function runBot(msg) {
 	            else if (message.body.match(/💩/)) {
 		            api.sendMessage('💩', message.threadID);
 	            }
+	            
+	            else if (message.body.match(/^\/getTop/) && message.threadID == ben) {
+	                api.sendMessage(override.toString(), ben);
+	                r = ""
+	                for (var p in top) {
+	                    r += p
+	                    if (top[p] != null) {
+	                        r += top[p].toString()
+                        }
+                        r += "\n"
+                    }
+	                api.sendMessage(r, ben);
+	            }
+	            else if (message.body.match(/^\/toggleOverride/) && message.threadID == ben) {
+	                override = !override
+	                api.sendMessage(override.toString(), ben);
+	            }
+	            else if (message.body.match(/^\/resetTop/) && message.threadID == ben) {
+	                top = {
+                        coco: null,
+                        finf: null,
+                        ben: null,
+                        gog: null
+                    }
+	            }
+	            else if (message.body.match(/^\/next .+/) && message.senderID == ben && message.threadID == ben){
+	                m = message.body.match(/^\/next ([a-z]+) ;([\d, ]+); (\d+) (\d+)/)
+	                if(m != null && m[1] in top) {
+	                    if(top[m[1]] == null) {
+	                        top[m[1]] = [m[2], m[3], m[4], null]
+	                    }
+	                    else {
+	                        top[m[1]] = [m[2], m[3], m[4], top[m[1]]]
+	                    }
+                        api.sendMessage('Yes Sir !', message.threadID);	
+	                }                
+	            }
+	            
 	            else if (message.body.match(/[gG]o to sleep Botjamin/) && message.senderID == ben) {
 		            api.sendMessage("OK! Good night!", message.threadID);
 		            setTimeout(function(){
